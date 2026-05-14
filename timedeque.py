3d3fba25297e5fd6d0f52272e1ec0b08c2c86999
Achilles Moraitis <achillesm2004@gmail.com>
2026-05-14 18:20:49 -0400

    fdgdfgd

---
+++ timedeque.py
@@ -0,0 +1,41 @@
+from time import monotonic
+from collections import deque
+
+class TimeDeque:
+    def __init__(self, seconds):
+        self.seconds = seconds
+        self.items = deque()
+
+    def append(self, item) -> None:
+        now = monotonic()
+        self.items.append((now, item))
+        self._expire(now)
+
+    def _expire(self, now=None):
+        now = monotonic() if now is None else now
+        cutoff = now - self.seconds
+        while self.items and self.items[0][0] < cutoff:
+            self.items.popleft()
+
+    def __iter__(self):
+        self._expire()
+        return (item for _, item in self.items)
+
+    def __getitem__(self, index):
+        self._expire()
+        return self.items[index][1]
+
+    def __len__(self) -> int:
+        self._expire()
+        return len(self.items)
+
+    def __bool__(self) -> bool:
+        self._expire()
+        return bool(self.items)
+
+    def clear(self) -> None:
+        self.items.clear()
+
+    def to_list(self) -> list:
+        self._expire()
+        return [item for _, item in self.items]
